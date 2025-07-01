f8a6a2880a96d92babd644b7743dc17dade7bfca
feldora <fikri@cumi-cumi>
2025-07-01 21:52:35 +0800

    FIX create user

--- app/Http/Controllers/Web/UserManagementController.php
+++ app/Http/Controllers/Web/UserManagementController.php
@@ -39,7 +39,7 @@ class UserManagementController extends Controller
 
         $user->syncRoles($request->roles);
 
-        return redirect()->route('/admin/users')->with('success', 'User berhasil dibuat');
+        return redirect()->route('admin.users.index')->with('success', 'User berhasil dibuat');
     }
 
     public function edit(User $user)
@@ -69,12 +69,12 @@ class UserManagementController extends Controller
 
         $user->syncRoles($request->roles);
 
-        return redirect()->route('/admin/users')->with('success', 'User berhasil diperbarui');
+        return redirect()->route('admin.users.index')->with('success', 'User berhasil diperbarui');
     }
 
     public function destroy(User $user)
     {
         $user->delete();
-        return redirect()->route('/admin/users')->with('success', 'User berhasil dihapus');
+        return redirect()->route('admin.users.index')->with('success', 'User berhasil dihapus');
     }
 }
